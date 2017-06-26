01f0e476b97e90672b9a59214fdb57148cd77f01
azmeer <azmeer.salleh@gmail.com>
2017-06-26 16:15:53 -0700

    disables comma-dangle linting rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,8 @@ module.exports = {
   extends: [
     './node_modules/eslint-config-hackreactor/index.js',
     './node_modules/eslint-config-airbnb/index.js' 
-  ]
+  ],
+  rules: {
+    'comma-dangle': 'off'
+  }
 };
