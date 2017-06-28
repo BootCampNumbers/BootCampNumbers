dc85fe3d6252b559032ece658d91169dde50e97e
Sheyda Rezaei <sheydarezaee@gmail.com>
2017-06-28 10:37:39 -0700

    Adds outcomes routes

--- server/routes/outcomes.js
+++ server/routes/outcomes.js
@@ -1,7 +1,36 @@
+// const db = require('../../database/index.js');
 const router = require('express').Router();
 
-// router.get('/', (req, res, next) => {
-//   do something here
+router.get('/', (req, res, next) => {
+  // console.log('Router GET to homepage works');
+  // const options = {
+  //   bootcamp : req.query.bootcamp,
+  //   campus : req.query.campus
+  // };
+  // db.getOutcomes(options);
+  next();
+});
+
+router.get('/api/outcomes/bootcamp/:name', (req, res, next) => {
+  res.send('Router GET to /api/outcomes/bootcamp/:name works');
+  // db.getOutcomes({ bootcamp : req.params.name});
+  next();
+});
+
+router.get('/api/outcomes/city/:city', (req, res, next) => {
+  console.log('Router GET to /api/outcomes/city/:city works');
+  // db.getOutcomes({ bootcamp : req.params.city});
+  next();
+});
+
+router.get('/api/outcomes/company/:company', (req, res, next) => {
+  console.log('Router GET to /api/outcomes/company/:company works');
+  // db.getOutcomes({ bootcamp : req.params.company});
+  next();
+});
+
+// router.post('/api/outcomes', (req, res, next) => {
 // });
 
 module.exports = router;
+
