0e198522fc93c8a37e5cc8cb5ac4b4d015bd4d8d
Wesley Choi <weschoi.creative@gmail.com>
2017-06-28 11:17:26 -0700

    start auth

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const morgan = require('morgan-body');
-var GitHubStrategy = require('passport-github2').Strategy;
+const GitHubStrategy = require('passport-github2').Strategy;
 
 const outcomes = require('./routes/outcomes');
 
@@ -30,41 +30,34 @@ app.use(express.static(path.join(__dirname, './../node_modules')));
 app.use(passport.initialize());
 
 passport.use(new GitHubStrategy({
-    clientID: '44a184746a6deb0596da',
-    clientSecret: 'f26b3b0b9cee59afdc0e486e4fc4757fc4deb15d',
-    callbackURL: "https://glasscamp-production.herokuapp.com/register"
-  },
-  function(accessToken, refreshToken, profile, cb) {
+  clientID: '44a184746a6deb0596da',
+  clientSecret: 'f26b3b0b9cee59afdc0e486e4fc4757fc4deb15d',
+  callbackURL: 'https://glasscamp-production.herokuapp.com/register'
+},
+  (accessToken, refreshToken, profile, cb) => {
     console.log('profile', profile);
     console.log('accessToken', accessToken);
-    Users.findOrCreate({ githubId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
+    cb(null);
+    // Users.findOrCreate({ githubId: profile.id }, function (err, user) {
+    //   return cb(err, user);
   }
 ));
-
-app.get('/auth', 
+app.get('/auth',
   passport.authenticate('github', { scope: ['user:email'] }),
-  function(req, res) {
+  (req, res) => {
     console.log('/auth');
     // Successful authentication, redirect home.
     res.redirect('/');
-  });
-
-
+  }
+);
 
 app.get('/register', (req, res) => {
   console.log(req.body);
   res.send('cool');
   // console.log('clicked');
-})
+});
 
 /* eslint-disable no-console */
 app.listen(PORT, () =>
   console.log(`GlassCamp server listening on port ${PORT}`));
 /* eslint-enable no-console */
-
-
-
-
-
