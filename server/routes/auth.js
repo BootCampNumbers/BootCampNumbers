8f2b064a1437b5e7afc902cef0c92532a723de92
Sheyda Rezaei <sheydarezaee@gmail.com>
2017-06-29 14:01:24 -0700

    add user on auth route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,20 @@
-// const db = require('../../database/index.js');
-
-// const router = require('express').Router();
-
-
-  
-
+const db = require('../../database/index.js');
+const router = require('express').Router();
 
 // router.get('/api/auth/github',
 //   passport.authenticate('github', { scope: ['user:email'] }),
 //   function(req, res) {});
+
+router.get('/auth/github/callback', (req, res, next) => {
+  const options = {
+    firstName: 'Kristy',
+    lastName: 'Azad', 
+    email: 'kristyazad@gmail.com' 
+  };
+  db.users.setUser(options, user => {
+    console.log('Server addUser', user);
+    res.send(user);
+  });
+});
+
+module.exports = router;
