2f40abe802af97b256724fab008de4d8a1f913a6
azmeer <azmeer.salleh@gmail.com>
2017-06-28 23:29:32 -0700

    enables cors() for routes

--- server/index.js
+++ server/index.js
@@ -25,18 +25,19 @@ const PORT = process.env.PORT || 1337;
 
 const app = express();
 
-app.use(bodyParser.json());
 morgan(app);
+
+app.use(bodyParser.json());
 
 app.use('*', cors());
 
-app.use('/api/outcomes', outcomes);
-
 // app.use('/api/auth', auth);
-
-app.use(express.static(path.join(__dirname, './../client/dist')));
 // app.use(passport.initialize());
 // app.use(passport.session());
+
+app.use('/api/outcomes', outcomes);
+
+app.use(express.static(path.join(__dirname, './../client/dist')));
 
 /* eslint-disable no-console */
 app.listen(PORT, () =>
